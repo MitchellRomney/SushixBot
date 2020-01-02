a171e145aa988b82bc1948f787d065f3b9a208f3
Mitchell Romney <mitchell@theearthissquare.com>
2020-01-03 00:52:36 +1100

    fix(webhooks): add json loading to the /followers response body

--- Twitch/views.py
+++ Twitch/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -8,17 +10,14 @@ def followers(request):
 
     if request.method == 'GET':
         if "hub.challenge" in request.GET:
-            print('Confirming /followers Webhook!')
             return HttpResponse(request.GET["hub.challenge"], status=200)
 
     elif request.method == 'POST':
-        print('Received POST request!')
-        print(request)
-        print(request.body)
-        if "data" in request.body:
-            print(request.body["data"])
-            print(request.body["data"][0])
-            payload = request.body["data"][0]
+        body = json.loads(request.body)
+        if "data" in body:
+            print(body["data"])
+            print(body["data"][0])
+            payload = body["data"][0]
             print(f'New follower: {payload["from_name"]}, Twitch ID: {payload["from_id"]}')
 
     return HttpResponse(status=204)
